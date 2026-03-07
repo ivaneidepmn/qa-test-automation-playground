e44cadd120861b4fc5f3700e0d9e10a12f50aca6
Ivaneide Pires Monteiro do Nascimento <ivapmn@gmail.com>
2026-03-07 02:32:30 -0300

    chore: configure Cypress reporter (mochawesome) and update test configuration

--- examples/cypress-demo/cypress.config.js
+++ examples/cypress-demo/cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "reports",
+    overwrite: false,
+    html: true,
+    json: true
+  },
+
   e2e: {
     baseUrl: "https://www.saucedemo.com",
     viewportWidth: 1366,
     viewportHeight: 768,
+
     setupNodeEvents(on, config) {
       return config;
     },
